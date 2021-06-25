99832828802b20aa84417c99211adadc8c9cb85c
Mike Shohet <mlshohet@gmail.com>
2021-06-25 16:03:16 -0400

    added question widget, styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget>createState() {
 
-  void answerQuestion() {
-    print('Answer chosen');
+    return _MyAppState();
+  }
+}
+
+//add a pointer to the app class in <> brackets
+class _MyAppState extends State<MyApp> {
+
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override // Dart overrides the StateLessWidget build method
   Widget build(BuildContext context) {
     
     var questions = [
-      'What\'s your favorite coor?',
+      'What\'s your favorite color?',
       'What\'s your favorite animal',
     ];
 
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text("My Flutter App"),),
+      appBar: AppBar(title: Text("First App")),
       body: Column(children: <Widget>[
-          Text('The question!'),
-            RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion,),
-            RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion,),
-            RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion,),
+          Question(questions[_questionIndex],),
+            RaisedButton(child: Text("Answer 1"), onPressed: _answerQuestion,),
+            RaisedButton(child: Text("Answer 2"), onPressed: _answerQuestion,),
+            RaisedButton(child: Text("Answer 3"), onPressed: _answerQuestion,),
           ],
         ),
       ),
